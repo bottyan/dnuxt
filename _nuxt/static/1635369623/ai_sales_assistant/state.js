74793771542899c3759b315ef66409807b7e1635
bottyan <bottyan@users.noreply.github.com>
2021-10-27 21:20:23 +0000

    deploy: 593ed8827eb7e6abfd10b2708461c224b6bb8942

---
+++ _nuxt/static/1635369623/ai_sales_assistant/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdnuxt\u002F_nuxt\u002Fstatic\u002F1635369623",layout:"dialogity",error:a,serverRendered:true,routePath:"\u002Fai_sales_assistant",config:{_app:{basePath:"\u002Fdnuxt\u002F",assetsPath:"\u002Fdnuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
