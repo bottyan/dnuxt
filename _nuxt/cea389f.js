b8f145fd3278d5ff2a670732cba8de3e49136d69
bottyan <bottyan@users.noreply.github.com>
2021-11-05 19:18:27 +0000

    deploy: 75d6a7eab6f610bb99d2de11f878ed95608fcc82

--- _nuxt/cea389f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{250:function(n,t,e){"use strict";e.r(t);var l={layout:"dialogity"},o=e(27),component=Object(o.a)(l,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",[n._v("\n  AI Sales Assistant\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
