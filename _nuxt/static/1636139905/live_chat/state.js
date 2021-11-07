288d1778f54d6a0fdb09a3cb1cdbdb34e5ea638f
bottyan <bottyan@users.noreply.github.com>
2021-11-07 09:21:53 +0000

    deploy: 527241a0495e6aaedcc6eb8b32567de78178e905

--- _nuxt/static/1636139905/live_chat/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdnuxt\u002F_nuxt\u002Fstatic\u002F1636139905",layout:"dialogity",error:a,serverRendered:true,routePath:"\u002Flive_chat",config:{_app:{basePath:"\u002Fdnuxt\u002F",assetsPath:"\u002Fdnuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
