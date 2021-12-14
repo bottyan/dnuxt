28ec33b6dfc0484713b27789e88ddb95705b0e7d
bottyan <bottyan@users.noreply.github.com>
2021-12-14 08:41:36 +0000

    deploy: 2588d1b292133ed69855bbcab53effa6fd33f283

--- _nuxt/static/1638779130/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdnuxt\u002F_nuxt\u002Fstatic\u002F1638779130",layout:"dialogity",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdnuxt\u002F",assetsPath:"\u002Fdnuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
