e39d7873589c57aa555fecf74b78c0b8178be4df
bottyan <bottyan@users.noreply.github.com>
2022-01-21 14:31:24 +0000

    deploy: 4b592d58219f21e4804bfcfd78e4641708010f86

---
+++ _nuxt/static/1642775483/pricing/premium-details/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdnuxt\u002F_nuxt\u002Fstatic\u002F1642775483",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fdnuxt\u002F",assetsPath:"\u002Fdnuxt\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fpricing\u002Fpremium-details"));
