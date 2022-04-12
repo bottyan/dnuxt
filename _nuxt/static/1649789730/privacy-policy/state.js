6e46c350638085a893ac6e584dc4e211d29a45ee
bottyan <bottyan@users.noreply.github.com>
2022-04-12 18:55:31 +0000

    deploy: 0775c2c756eb336c1aeee69ca0d6e120989191cd

---
+++ _nuxt/static/1649789730/privacy-policy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdnuxt\u002F_nuxt\u002Fstatic\u002F1649789730",layout:"dialogity",error:a,serverRendered:true,routePath:"\u002Fprivacy-policy",config:{_app:{basePath:"\u002Fdnuxt\u002F",assetsPath:"\u002Fdnuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
